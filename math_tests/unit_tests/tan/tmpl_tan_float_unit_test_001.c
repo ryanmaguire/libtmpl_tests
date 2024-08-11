e1788cfa4eba5a1acb67907e81b8cdc22536fc71
ryanmaguire <rmaguire@protonmail.com>
2024-08-11 11:36:03 -0400

    More tests.

---
+++ math_tests/unit_tests/tan/tmpl_tan_float_unit_test_001.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl_tests.                                       *
+ *                                                                            *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+#ifndef TMPL_NSAMPS
+#define TMPL_NSAMPS (1E7)
+#endif
+#include "../../../libtmpl_tests.h"
+TMPL_TEST_REAL_FUNC_VS_REAL_FUNC_UNIT_TEST(
+    float, -1.0E6F, 1.0E6F, tmpl_Float_Tan, tanf
+)
