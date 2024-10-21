8c8f54df2c4d1a4a649d2ce705ddb5cd9639e75a
ryanmaguire <rmaguire@protonmail.com>
2024-10-21 10:42:48 -0400

    Organizing helper scripts.

---
+++ helper/runtests.sh
@@ -0,0 +1,89 @@
+################################################################################
+#                                   LICENSE                                    #
+################################################################################
+#   This file is part of libtmpl_tests.                                        #
+#                                                                              #
+#   libtmpl_tests is free software: you can redistribute it and/or modify      #
+#   it under the terms of the GNU General Public License as published by       #
+#   the Free Software Foundation, either version 3 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   libtmpl_tests is distributed in the hope that it will be useful,           #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.    #
+################################################################################
+#   Author:     Ryan Maguire                                                   #
+#   Date:       October 21, 2024                                               #
+################################################################################
+
+# Runs all tests in a given directory and of a given type.
+#   Example Usage:
+#       runtests -cc=gcc -cpp=clang++ -type=unit -Wall -Wextra -Wpedantic
+#   Arguments:
+#       -cc:
+#           Any C compiler supporting C99 and C11 extensions. The tests make
+#           use of the _Generic keyword and variadic macros, so while the
+#           the core of libtmpl is C89 compliant (and C99, C11, and C18), the
+#           tests are not.
+#       -cpp:
+#           Any C++ compiler.
+#       -type:
+#           The type of tests you wish to run. Options are:
+#               -type=unit
+#               -type=time
+#       -ExtraArgs:
+#           Space separated arguments for the C and C++ compilers. For example:
+#               -Wall -Wextra -Wpedantic
+# All arguments are optional. Default values are:
+#   -cc=gcc
+#   -cpp=g++
+#   -type=unit
+runtests() {
+    CC=gcc
+    CPP=g++
+    ExtraArgs=""
+    TYPE="unit"
+
+    # Parse the inputs.
+    for arg in "$@"; do
+
+        # If there are no more arguments, break out of this loop.
+        if [ "$arg" == "" ]; then
+            break
+
+        # Check if the user wants to use a different C compiler.
+        elif [[ "$arg" == *"-cc"* ]]; then
+            CC=${arg#*=}
+
+        # Check if the user wants to use a different C++ compiler.
+        elif [[ "$arg" == *"-cpp"* ]]; then
+            CPP=${arg#*=}
+
+        # Check which type of test we're running.
+        elif [[ "$arg" == *"-type"* ]]; then
+            TYPE=${arg#*=}
+
+        # The user can add compiler options if they want.
+        else
+            ExtraArgs="$ExtraArgs ${arg#*}"
+        fi
+    done
+
+    for file in $(find . -name "*$TYPE*.c" -type f); do
+        $CC $ExtraArgs -O3 -flto $file -o main -lm -ltmpl
+        printf "$(basename $file): "
+        ./main
+        rm -f main
+    done
+
+    for file in $(find . -name "*$TYPE*.cpp" -type f); do
+        $CPP $ExtraArgs -O3 -flto $file -o main -ltmpl
+        printf "$(basename $file): "
+        ./main
+        rm -f main
+    done
+}
