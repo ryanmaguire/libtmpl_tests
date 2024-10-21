2fd6d0c3c64549740aca3ac0a314dacb02345b91
ryanmaguire <rmaguire@protonmail.com>
2024-10-21 09:06:24 -0400

    Adding more unit test macros.

--- libtmpl_tests.h
+++ libtmpl_tests.h
@@ -397,6 +397,30 @@ int main(void)                                                                 \
     return 0;                                                                  \
 }
 
+#define TMPL_REAL_BOOL_FUNC_ARRAY_VS_ANSWER_EXACT(type, func, indata, outdata) \
+int main(void)                                                                 \
+{                                                                              \
+    const type in[] = indata;                                                  \
+    const tmpl_Bool out[] = outdata;                                           \
+    const size_t number_of_samples = NSAMPS(type);                             \
+    const size_t zero = TMPL_CAST(0, size_t);                                  \
+    size_t n;                                                                  \
+    for (n = zero; n < number_of_samples; ++n)                                 \
+    {                                                                          \
+        const tmpl_Bool output = func(in[n]);                                  \
+        if (output != out[n])                                                  \
+        {                                                                      \
+            puts("FAIL");                                                      \
+            printf("    Input   = %+.40LE\n", TMPL_CAST(in[n], double));       \
+            printf("    libtmpl = %+.40LE\n", TMPL_CAST(output, double));      \
+            printf("    Other   = %+.40LE\n", TMPL_CAST(out[n], double));      \
+            return -1;                                                         \
+        }                                                                      \
+    }                                                                          \
+    puts("PASS");                                                              \
+    return 0;                                                                  \
+}
+
 #define TMPL_TEST_REAL_FROM_CSV_UNIT_TEST(type, csv, f)                        \
 int main(void)                                                                 \
 {                                                                              \
@@ -446,7 +470,9 @@ int main(void)                                                                 \
         const type output = func(in[n]);                                       \
         const tmpl_Bool val_is_nan = TMPL_IS_NAN(output);                      \
         const tmpl_Bool out_is_nan = TMPL_IS_NAN(out[n]);                      \
-        if ((val_is_nan != out_is_nan) || (output != out[n]))                  \
+        if (out_is_nan && val_is_nan)                                          \
+            continue;                                                          \
+        else if ((val_is_nan != out_is_nan) || (output != out[n]))             \
         {                                                                      \
             puts("FAIL");                                                      \
             printf("    Input   = %+.40LE\n", TMPL_CAST(in[n], long double));  \
