dedabf9d9ada52785adacc89db50ec777846c1f5
ryanmaguire <rmaguire@protonmail.com>
2024-10-21 11:26:34 -0400

    Some more tests.

---
+++ math_tests/unit_tests/min/tmpl_min_double_unit_test_001.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl_tests.                                       *
+ *                                                                            *
+ *  libtmpl_tests is free software: you can refminribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl_tests is fminributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+#ifndef TMPL_NSAMPS
+#define TMPL_NSAMPS (1E7)
+#endif
+#include "../../../libtmpl_tests.h"
+TMPL_TEST_REAL2_FUNC_VS_REAL2_FUNC_UNIT_TEST(
+    double, -1.0E6, 1.0E6, tmpl_Double_Min, fmin
+)
