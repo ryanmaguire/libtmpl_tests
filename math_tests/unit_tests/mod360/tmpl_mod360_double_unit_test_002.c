1bc310b34072eb715ed5c732606ee11859bf3dfc
ryanmaguire <rmaguire@protonmail.com>
2024-10-21 14:21:56 -0400

    Some more tests.

---
+++ math_tests/unit_tests/mod360/tmpl_mod360_double_unit_test_002.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl_tests.                                       *
+ *                                                                            *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+#ifndef TMPL_NSAMPS
+#define TMPL_NSAMPS (1E7)
+#endif
+#include "../../../libtmpl_tests.h"
+static double mod360(double x){return fmod(x, 360.0);}
+TMPL_TEST_REAL_FUNC_VS_REAL_FUNC_UNIT_TEST(
+    double, 0.0, 1.0E1, tmpl_Double_Mod_360, mod360
+)
