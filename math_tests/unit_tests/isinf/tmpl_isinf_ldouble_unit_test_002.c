0fbd7ea0a601897aec98f76fce4a69130970ad9c
ryanmaguire <rmaguire@protonmail.com>
2024-10-21 09:06:13 -0400

    Fixing more unit tests.

--- math_tests/unit_tests/isinf/tmpl_isinf_ldouble_unit_test_002.c
+++ math_tests/unit_tests/isinf/tmpl_isinf_ldouble_unit_test_002.c
@@ -1,25 +1,27 @@
 /******************************************************************************
  *                                  LICENSE                                   *
  ******************************************************************************
- *  This file is part of libtmpl.                                             *
+ *  This file is part of libtmpl_tests.                                       *
  *                                                                            *
- *  libtmpl is free software: you can redistribute it and/or modify it        *
- *  under the terms of the GNU General Public License as published by         *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by      *
  *  the Free Software Foundation, either version 3 of the License, or         *
  *  (at your option) any later version.                                       *
  *                                                                            *
- *  libtmpl is distributed in the hope that it will be useful,                *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
  *  GNU General Public License for more details.                              *
  *                                                                            *
  *  You should have received a copy of the GNU General Public License         *
- *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
  ******************************************************************************
  *  Author:     Ryan Maguire                                                  *
  *  Date:       September 5, 2022                                             *
  ******************************************************************************/
-#include "tmpl_math_unit_tests.h"
-#define indata {0.0, -0.0, DBL_MIN, -DBL_MIN, DBL_MAX, -DBL_MAX}
+#include "../../../libtmpl_tests.h"
+#define indata {0.0L, -0.0L, LDBL_MIN, -LDBL_MIN, LDBL_MAX, -LDBL_MAX}
 #define outdata {F, F, F, F, F, F}
-TEST2(double, tmpl_Double_Is_Inf, indata, outdata)
+TMPL_REAL_BOOL_FUNC_ARRAY_VS_ANSWER_EXACT(
+    long double, tmpl_LDouble_Is_Inf, indata, outdata
+)
