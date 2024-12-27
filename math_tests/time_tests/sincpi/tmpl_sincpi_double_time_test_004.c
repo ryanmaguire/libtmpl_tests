3b92c4661a3fcc51d1aa9d3331a6081cc623116c
ryanmaguire <rmaguire@protonmail.com>
2024-12-27 12:44:56 -0500

    Some time tests for sincpi.

---
+++ math_tests/time_tests/sincpi/tmpl_sincpi_double_time_test_004.c
@@ -0,0 +1,34 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl_tests.                                       *
+ *                                                                            *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+#ifndef TMPL_NSAMPS
+#define TMPL_NSAMPS (3E8)
+#endif
+#include "../../../libtmpl_tests.h"
+static double sincpi(double x)
+{
+    double arg;
+    if (x == 0.0)
+        return 1.0;
+
+    arg = tmpl_Double_Pi *x;
+    return sin(arg) / arg;
+}
+TMPL_TEST_REAL_FUNC_VS_REAL_FUNC_TIME_TEST(
+    double, -5.0E-01, 5.0E-01, tmpl_Double_SincPi, sincpi
+)
