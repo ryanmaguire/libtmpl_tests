5d590dc55e2266fb3533bb6cc301be3d3259c97c
ryanmaguire <rmaguire@protonmail.com>
2025-01-31 15:19:25 -0500

    Fixing arcsin time tests.

--- math_tests/time_tests/arcsin/tmpl_arccos_ldouble_time_test_denormal_vals.c
+++ math_tests/time_tests/arcsin/tmpl_arccos_ldouble_time_test_denormal_vals.c
@@ -1,21 +1,25 @@
 /******************************************************************************
  *                                  LICENSE                                   *
  ******************************************************************************
- *  This file is part of libtmpl.                                             *
+ *  This file is part of libtmpl_tests.                                       *
  *                                                                            *
- *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
  *  it under the terms of the GNU General Public License as published by      *
  *  the Free Software Foundation, either version 3 of the License, or         *
  *  (at your option) any later version.                                       *
  *                                                                            *
- *  libtmpl is distributed in the hope that it will be useful,                *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
  *  GNU General Public License for more details.                              *
  *                                                                            *
  *  You should have received a copy of the GNU General Public License         *
- *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
  ******************************************************************************/
-#include "tmpl_math_time_tests.h"
-#include <libtmpl/include/math/tmpl_arcsin_pade_ldouble.h>
-TEST1(long double, -0.5L, 0.5L, tmpl_LDouble_Arcsin_Pade, asinl)
+#ifndef TMPL_NSAMPS
+#define TMPL_NSAMPS (1E8)
+#endif
+#include "../../../libtmpl_tests.h"
+TMPL_TEST_REAL_FUNC_VS_REAL_FUNC_TIME_TEST(
+    long double, -LDBL_MIN, LDBL_MIN, tmpl_LDouble_Arcsin, asinl
+)
