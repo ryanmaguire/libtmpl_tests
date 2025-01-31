69eb220266f787c16c2d12fa06780816bc711b09
ryanmaguire <rmaguire@protonmail.com>
2025-01-31 15:25:59 -0500

    Fixing cbrt tests.

--- math_tests/time_tests/cbrt/tmpl_cbrt_double_time_test.c
+++ math_tests/time_tests/cbrt/tmpl_cbrt_double_time_test.c
@@ -1,20 +1,25 @@
 /******************************************************************************
  *                                  LICENSE                                   *
  ******************************************************************************
- *  This file is part of libtmpl.                                             *
+ *  This file is part of libtmpl_tests.                                       *
  *                                                                            *
- *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
  *  it under the terms of the GNU General Public License as published by      *
  *  the Free Software Foundation, either version 3 of the License, or         *
  *  (at your option) any later version.                                       *
  *                                                                            *
- *  libtmpl is distributed in the hope that it will be useful,                *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
  *  GNU General Public License for more details.                              *
  *                                                                            *
  *  You should have received a copy of the GNU General Public License         *
- *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
  ******************************************************************************/
-#include "tmpl_math_time_tests.h"
-TEST1(double, -1.0E6, 1.0E6, tmpl_Double_Cbrt, cbrt)
+#ifndef TMPL_NSAMPS
+#define TMPL_NSAMPS (1E8)
+#endif
+#include "../../../libtmpl_tests.h"
+TMPL_TEST_REAL_FUNC_VS_REAL_FUNC_TIME_TEST(
+    double, -1.0, 1.0, tmpl_Double_Cbrt, cbrt
+)
