d36a1879b7fcfd6f4b5cb39feb7e5610fe8c8b53
ryanmaguire <rmaguire@protonmail.com>
2025-05-06 17:09:15 -0400

    Adding vec2 tests.

---
+++ vec2_tests/unit_tests/l2norm/tmpl_vec2_l2_norm_float_unit_test_x_hat_big_num.c
@@ -0,0 +1,29 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl_tests.                                       *
+ *                                                                            *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+#include "../../../libtmpl_tests.h"
+#define inarr {         \
+    {{+BNUMF, +0.0F}},  \
+    {{+BNUMF, -0.0F}},  \
+    {{-BNUMF, +0.0F}},  \
+    {{-BNUMF, -0.0F}}   \
+}
+#define outarr {BNUMF, BNUMF, BNUMF, BNUMF}
+TMPL_TWOVEC_TO_R_ARRAY_UNIT_TEST(
+    tmpl_TwoVectorFloat, float, tmpl_2DFloat_L2_Norm, inarr, outarr
+)
