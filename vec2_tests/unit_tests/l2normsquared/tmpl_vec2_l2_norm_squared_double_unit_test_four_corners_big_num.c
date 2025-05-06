a20d66659d6be8d8e862117f8e9d69ae13d8747d
ryanmaguire <rmaguire@protonmail.com>
2025-05-06 17:29:15 -0400

    More unit tests for vec2 functions.

---
+++ vec2_tests/unit_tests/l2normsquared/tmpl_vec2_l2_norm_squared_double_unit_test_four_corners_big_num.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl_tests.                                       *
+ *                                                                            *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+#include "../../../libtmpl_tests.h"
+#define IN_VAL 0.5 * tmpl_Double_Sqrt(BNUM)
+#define OUT_VAL 0.5 * BNUM
+#define inarr {             \
+    {{+IN_VAL, +IN_VAL}},   \
+    {{+IN_VAL, -IN_VAL}},   \
+    {{-IN_VAL, +IN_VAL}},   \
+    {{-IN_VAL, -IN_VAL}}    \
+}
+#define outarr {OUT_VAL, OUT_VAL, OUT_VAL, OUT_VAL}
+TMPL_TWOVEC_TO_R_ARRAY_UNIT_TEST(
+    tmpl_TwoVectorDouble, double, tmpl_2DDouble_L2_Norm_Squared, inarr, outarr
+)
