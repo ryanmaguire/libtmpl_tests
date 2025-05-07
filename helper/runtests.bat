bb19cb4b0415d756e8c932064da85234625b3896
ryanmaguire <rmaguire@protonmail.com>
2025-05-06 20:54:50 -0400

    Improving Windows unit test script.

--- helper/runtests.bat
+++ helper/runtests.bat
@@ -1,5 +1,5 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-::                                 LICENSE                                    ::
+::                                  LICENSE                                   ::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 ::  This file is part of libtmpl_tests.                                       ::
 ::                                                                            ::
@@ -20,20 +20,75 @@
 ::  Date:       January 23, 2025                                              ::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @ECHO OFF
+SETLOCAL EnableDelayedExpansion
 
-:: Compiler arguments. Unit tests used _Generic and variadic macros to
+:: Default values, use MSVC (cl.exe).
+SET "CC=cl"
+SET "CPP=cl"
+SET "ExtraArgs="
+SET "TYPE=unit"
+
+:: C Compiler arguments. Unit tests use _Generic and variadic macros to
 :: simplify things (libtmpl itself does not, it is C89 compliant). Need to
 :: enable std:c11 for things to compile correctly.
-SET CARGS=/std:c11 /O2 /IC:\
+SET "CARGS=/std:c11 /O2 /IC:\"
 
-:: Linker / output arguments.
-SET LARGS=/link /out:main.exe
+:: C++ unit tests makes use of C++17 standard library functions. Enable these.
+SET "CPPARGS=/std:c++17 /O2 /IC:\"
 
-FOR /D %%d IN (.\*) DO (
-    FOR %%i IN (%%d\*unit_test*.c) DO (
-        cl %CARGS% %%i C:\libtmpl\libtmpl.lib %LARGS%
-        ECHO %%i: >> results.txt
-        main.exe >> results.txt
-        DEL *.obj *.exe
+:: Linker arguments (the last bit silences the compiler / linker).
+SET "LARGS=/link /out:main.exe >nul 2>&1"
+
+:: Parse command-line arguments.
+:PARSE_ARGS
+IF "%~1"=="" GOTO DONE_ARGS
+
+ECHO %~1 | FINDSTR /b /c:"-cc=" >NUL
+IF NOT ERRORLEVEL 1 (
+    FOR /f "tokens=2 delims==" %%a IN ("%~1") DO SET "CC=%%a"
+    SHIFT
+    GOTO PARSE_ARGS
+)
+
+ECHO %~1 | FINDSTR /b /c:"-cpp=" > NUL
+IF NOT ERRORLEVEL 1 (
+    FOR /f "tokens=2 delims==" %%a IN ("%~1") DO SET "CPP=%%a"
+    SHIFT
+    GOTO PARSE_ARGS
+)
+
+ECHO %~1 | FINDSTR /b /c:"-type=" > NUL
+IF NOT ERRORLEVEL 1 (
+    FOR /f "tokens=2 delims==" %%a IN ("%~1") DO SET "TYPE=%%a"
+    SHIFT
+    GOTO PARSE_ARGS
+)
+
+:: Any other arguments go to ExtraArgs.
+SET "ExtraArgs=!ExtraArgs! %~1"
+SHIFT
+GOTO PARSE_ARGS
+
+:DONE_ARGS
+
+:: Compile and run C files.
+FOR /f "delims=" %%f IN ('dir /b /s "*%TYPE%*.c"') DO (
+    CALL "%CC%" !ExtraArgs! %CARGS% "%%f" C:\libtmpl\libtmpl.lib %LARGS%
+    IF EXIST main.exe (
+        < NUL SET /p ="%%f: "
+        CALL main.exe
+        DEL /f /q main.exe *.obj
     )
 )
+
+:: Compile and run C++ files.
+FOR /f "delims=" %%f IN ('dir /b /s "*%TYPE%*.cpp"') DO (
+    CALL "%CC%" !ExtraArgs! %CPPARGS% "%%f" C:\libtmpl\libtmpl.lib %LARGS%
+    IF EXIST main.exe (
+        < NUL SET /p ="%%f: "
+        CALL main.exe
+        DEL /f /q main.exe *.obj
+    )
+)
+
+ENDLOCAL
