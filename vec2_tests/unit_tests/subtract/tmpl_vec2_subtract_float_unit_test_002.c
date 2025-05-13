cb2a19768d369d75b349807d8321c16f6ab53ecf
ryanmaguire <rmaguire@protonmail.com>
2025-05-13 19:29:03 -0400

    Adding unit tests for vector subtraction.

---
+++ vec2_tests/unit_tests/subtract/tmpl_vec2_subtract_float_unit_test_002.c
@@ -0,0 +1,43 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl_tests.                                       *
+ *                                                                            *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+#include "../../../libtmpl_tests.h"
+#define inarr0 {      \
+    {{+1.0F, 0.0F}},  \
+    {{+1.0F, 0.0F}},  \
+    {{-1.0F, 0.0F}},  \
+    {{-1.0F, 0.0F}}   \
+}
+
+#define inarr1 {      \
+    {{0.0F, +1.0F}},  \
+    {{0.0F, -1.0F}},  \
+    {{0.0F, +1.0F}},  \
+    {{0.0F, -1.0F}}   \
+}
+
+#define outarr {      \
+    {{+1.0F, -1.0F}}, \
+    {{+1.0F, +1.0F}}, \
+    {{-1.0F, -1.0F}}, \
+    {{-1.0F, +1.0F}}  \
+}
+
+TMPL_TWOVEC2_TO_TWOVEC_ARRAY_EXACT_UNIT_TEST(
+    tmpl_TwoVectorFloat, tmpl_2DFloat_Subtract, inarr0, inarr1, outarr
+)
