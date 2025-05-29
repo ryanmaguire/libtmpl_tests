80225b63366664054e651fa39f8b62b5501356b8
ryanmaguire <rmaguire@protonmail.com>
2025-05-29 10:04:43 -0400

    Adding imag part tests.

---
+++ complex_tests/unit_tests/imagpart/tmpl_complex_imag_part_ldouble_unit_test_four_corners_small_num.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl_tests.                                       *
+ *                                                                            *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+#include "../../../libtmpl_tests.h"
+#define val DNUML
+#define inarr {         \
+    {{+DNUML, +DNUML}}, \
+    {{+DNUML, -DNUML}}, \
+    {{-DNUML, +DNUML}}, \
+    {{-DNUML, -DNUML}}  \
+}
+#define outarr {val, -val, val, -val}
+TMPL_C_TO_R_ARRAY_UNIT_TEST(
+    tmpl_ComplexLongDouble, long double, tmpl_CLDouble_Imag_Part, inarr, outarr
+)
