8feddc7cb6afbdf0dc208a9054afe5ab71a3ab42
ryanmaguire <rmaguire@protonmail.com>
2025-05-29 10:25:49 -0400

    Adding addto tests.

---
+++ complex_tests/unit_tests/addto/tmpl_complex_addto_float_unit_test_dup_001.c
@@ -0,0 +1,36 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl_tests.                                       *
+ *                                                                            *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+#include "../../../libtmpl_tests.h"
+#define inarr {       \
+    {{+1.0F, +0.0F}}, \
+    {{+1.0F, -0.0F}}, \
+    {{-1.0F, +0.0F}}, \
+    {{-1.0F, -0.0F}}  \
+}
+
+#define outarr {     \
+    {{+2.0F, 0.0F}}, \
+    {{+2.0F, 0.0F}}, \
+    {{-2.0F, 0.0F}}, \
+    {{-2.0F, 0.0F}}  \
+}
+
+TMPL_C2PTR_DUP_ARRAY_EXACT_UNIT_TEST(
+    tmpl_ComplexFloat, tmpl_CFloat_AddTo, inarr, outarr
+)
