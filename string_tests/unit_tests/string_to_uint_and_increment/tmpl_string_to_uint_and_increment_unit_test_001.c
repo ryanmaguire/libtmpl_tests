c96f90a0873641e4b6cfa4aecfc344eb94deb01c
ryanmaguire <rmaguire@protonmail.com>
2025-10-13 13:13:22 -0400

    Adding more tests.

---
+++ string_tests/unit_tests/string_to_uint_and_increment/tmpl_string_to_uint_and_increment_unit_test_001.c
@@ -0,0 +1,33 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl_tests.                                       *
+ *                                                                            *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+#include <libtmpl/include/tmpl_string.h>
+#include <stdio.h>
+
+int main(void)
+{
+    const char * str = "   +123abc";
+    const unsigned int val = tmpl_String_To_UInt_And_Increment(&str);
+
+    if ((val != 123U) || (*str != 'a'))
+        puts("FAIL");
+    else
+        puts("PASS");
+
+    return 0;
+}
