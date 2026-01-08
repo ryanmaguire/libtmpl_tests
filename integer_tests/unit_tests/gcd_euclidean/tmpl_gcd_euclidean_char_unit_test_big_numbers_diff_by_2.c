5212012cce338c2eb1335a254272a97467c1858e
ryanmaguire <rmaguire@protonmail.com>
2026-01-08 13:58:19 -0500

    Adding signed char unit tests.

---
+++ integer_tests/unit_tests/gcd_euclidean/tmpl_gcd_euclidean_char_unit_test_big_numbers_diff_by_2.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl_tests.                                       *
+ *                                                                            *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+#include "../../../libtmpl_tests.h"
+#define indata {    \
+    {114, 116},     \
+    {115, 117},     \
+    {116, 118},     \
+    {117, 119},     \
+    {118, 120},     \
+    {119, 121},     \
+    {120, 122}      \
+}
+#define outdata {2, 1, 2, 1, 2, 1, 2}
+TMPL_R2_TO_R_ARRAY_EXACT_UNIT_TEST(
+    signed char, tmpl_Char_GCD_Euclidean, indata, outdata
+)
