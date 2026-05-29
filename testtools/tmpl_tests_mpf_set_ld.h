0ace1e883b5244f7557d590fb6694c83956bb7de
ryanmaguire <rmaguire@protonmail.com>
2026-05-29 16:45:56 -0400

    Fixing typo.

--- testtools/tmpl_tests_mpf_set_ld.h
+++ testtools/tmpl_tests_mpf_set_ld.h
@@ -27,7 +27,7 @@
 static inline void tmpl_tests_mpf_set_ld(mpf_t op, long double val)
 {
     char buffer[192];
-    sprintf(buffer, "%.1700LE", val);
+    sprintf(buffer, "%.170LE", val);
     mpf_set_str(op, buffer, 10);
 }
 
