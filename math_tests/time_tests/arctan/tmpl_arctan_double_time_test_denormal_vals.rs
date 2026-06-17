edf805e0bc5b46323d6b2ead46191842a9345b2e
ryanmaguire <rmaguire@protonmail.com>
2026-06-17 10:47:08 -0400

    Adding more tests.

---
+++ math_tests/time_tests/arctan/tmpl_arctan_double_time_test_denormal_vals.rs
@@ -0,0 +1,32 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl_tests.                                       *
+ *                                                                            *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+include!("../../../libtmpl_tests.rs");
+unsafe extern "C" {
+    safe fn tmpl_Double_Arctan(x: raw::c_double) -> raw::c_double;
+}
+
+tmpl_r_to_r_vs_from_interval_time_test!(
+    raw::c_double,
+    f64,
+    0.0f64,
+    f64::MIN_POSITIVE,
+    100_000_000,
+    tmpl_Double_Arctan,
+    f64::atan
+);
