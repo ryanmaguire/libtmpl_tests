567139745e36be0330e7bbee3e7817dc6b9dd399
ryanmaguire <rmaguire@protonmail.com>
2026-06-17 10:21:01 -0400

    Adding more time tests.

---
+++ math_tests/time_tests/abs/tmpl_abs_float_time_test_denormal_vals.rs
@@ -0,0 +1,32 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl_tests.                                       *
+ *                                                                            *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+include!("../../../libtmpl_tests.rs");
+unsafe extern "C" {
+    safe fn tmpl_Float_Abs(x: raw::c_float) -> raw::c_float;
+}
+
+tmpl_r_to_r_vs_from_interval_time_test!(
+    raw::c_float,
+    f32,
+    0.0f32,
+    f32::MIN_POSITIVE,
+    10_000_000,
+    tmpl_Float_Abs,
+    f32::abs
+);
