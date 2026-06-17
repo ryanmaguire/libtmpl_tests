b666854b99d657d7913ad32c05826f4e79a0f872
ryanmaguire <rmaguire@protonmail.com>
2026-06-17 11:03:06 -0400

    Updating tests.

--- math_tests/time_tests/cosd/tmpl_cosd_float_time_test.c
+++ math_tests/time_tests/cosd/tmpl_cosd_float_time_test.c
@@ -1,26 +1,32 @@
 /******************************************************************************
  *                                  LICENSE                                   *
  ******************************************************************************
- *  This file is part of libtmpl.                                             *
+ *  This file is part of libtmpl_tests.                                       *
  *                                                                            *
- *  libtmpl is free software: you can redistribute it and/or modify           *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
  *  it under the terms of the GNU General Public License as published by      *
  *  the Free Software Foundation, either version 3 of the License, or         *
  *  (at your option) any later version.                                       *
  *                                                                            *
- *  libtmpl is distributed in the hope that it will be useful,                *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
  *  GNU General Public License for more details.                              *
  *                                                                            *
  *  You should have received a copy of the GNU General Public License         *
- *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
  ******************************************************************************/
-#include "tmpl_math_time_tests.h"
+#ifndef TMPL_NSAMPS
+#define TMPL_NSAMPS (3E7)
+#endif
+#include "../../../libtmpl_tests.h"
 
-static inline float cosdf(float x)
+static inline float cosdf(const float x)
 {
-    return cosf(fmodf(x, 360.0F) * tmpl_One_Pi_F/180.0F);
+    return cosf(fmodf(x, 360.0F) * tmpl_float_pi / 180.0F);
+
 }
 
-TEST1(float, -1.0E1F, 1.0E1F, tmpl_Float_Cosd, cosdf)
+TMPL_TEST_REAL_FUNC_VS_REAL_FUNC_TIME_TEST(
+    float, -1.0E1F, 1.0E1F, tmpl_Float_Cosd, cosdf
+)
