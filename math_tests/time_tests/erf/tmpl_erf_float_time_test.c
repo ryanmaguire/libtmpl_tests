83950e6d8088ce17233e01a87e18a8c03e70c1d2
ryanmaguire <rmaguire@protonmail.com>
2026-06-17 11:06:47 -0400

    Updating erf tests.

--- math_tests/time_tests/erf/tmpl_erf_float_time_test.c
+++ math_tests/time_tests/erf/tmpl_erf_float_time_test.c
@@ -16,5 +16,10 @@
  *  You should have received a copy of the GNU General Public License         *
  *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
  ******************************************************************************/
-#include "tmpl_math_time_tests.h"
-TEST1(float, -4.0F, 4.0F, tmpl_Float_Erf, erff)
+#ifndef TMPL_NSAMPS
+#define TMPL_NSAMPS (3E7)
+#endif
+#include "../../../libtmpl_tests.h"
+TMPL_TEST_REAL_FUNC_VS_REAL_FUNC_TIME_TEST(
+    float, -4.0F, 4.0F, tmpl_Float_Erf, erff
+)
--- math_tests/time_tests/erf/tmpl_erf_ldouble_time_test.c
+++ math_tests/time_tests/erf/tmpl_erf_ldouble_time_test.c
@@ -16,5 +16,10 @@
  *  You should have received a copy of the GNU General Public License         *
  *  along with libtmpl.  If not, see <https://www.gnu.org/licenses/>.         *
  ******************************************************************************/
-#include "tmpl_math_time_tests.h"
-TEST1(long double, -6.5L, 6.5L, tmpl_LDouble_Erf, erfl)
+#ifndef TMPL_NSAMPS
+#define TMPL_NSAMPS (3E8)
+#endif
+#include "../../../libtmpl_tests.h"
+TMPL_TEST_REAL_FUNC_VS_REAL_FUNC_TIME_TEST(
+    long double, -6.5L, 6.5L, tmpl_LDouble_Erf, erfl
+)
