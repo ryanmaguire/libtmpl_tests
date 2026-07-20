98a4436c1d8b3672aa43f74631526c67e55c2336
ryanmaguire <rmaguire@protonmail.com>
2026-07-20 14:03:07 -0400

    More tests.

---
+++ affine_tests/unit_tests/slopeintercept/tmpl_slope_intercept_float_unit_test_one.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of libtmpl_tests.                                       *
+ *                                                                            *
+ *  libtmpl_tests is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  libtmpl_tests is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with libtmpl_tests.  If not, see <https://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+#include "../../../libtmpl_tests.h"
+#define indata {{1.0F, 1.0F}, {2.0F, 1.0F}, {1.5F, 2.5F}, {0.5F, 2.5F}}
+#define outdata {{{1.0F, 1.0F}}, {{2.0F, 1.0F}}, {{1.5F, 2.5F}}, {{0.5F, 2.5F}}}
+TMPL_R2_TO_TWOVEC_ARRAY_UNIT_TEST(
+    float,
+    tmpl_AffineFloat,
+    tmpl_AffFloat_Slope_Intercept,
+    indata,
+    outdata
+)
